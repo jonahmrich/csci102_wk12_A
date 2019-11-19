547b0b9915b725d44f9763284312ab73e48511ba
Jonah Rich <jrich@mymail.mines.edu>
2019-11-19 00:48:55 -0700

    save for function 3

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,3 +5,10 @@ def LoadFile(filename):
     with open(filename, 'r') as f:
         contents = f.readlines()
     PrintOutput(contents)
+
+def UpdateString(str1, str2, index):
+    str1a = str1[:index] + str2
+    str1b = str1[index + 1:]
+    str1 = str1a + str1b
+    PrintOutput(str1)
+    
