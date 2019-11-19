6ac12401e752b2493b6d56fd9e5443e97e7346d8
Jonah Rich <jrich@mymail.mines.edu>
2019-11-19 01:23:50 -0700

    save for function 5

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,10 +10,17 @@ def UpdateString(str1, str2, index):
     str1a = str1[:index] + str2
     str1b = str1[index + 1:]
     str1 = str1a + str1b
-    return str1
+    PrintOutput(str1)
 
 def FindWordCount(file_list, word):
     file_str = ' '.join(file_list)
     count = file_str.count(word.lower()) + file_str.count(word.capitalize())
     return count
-    
+
+def ScoreFinder(names, scores, name):
+    if name.lower() in names:
+        PrintOutput('%s got a score of %d' %(name.capitalize(), scores[names.index(name.lower())]))
+    elif name.capitalize() in names:
+        PrintOutput('%s got a score of %d' %(name.capitalize(), scores[names.index(name.capitalize())]))
+    else:
+        PrintOutput('player not found')
